354249037457db417b9a159cb1f1a251b2ae6c2c
Arsalan Sadri <asadri62@gmail.com>
2018-10-07 21:36:30 -0700

    added the code to grab all events associated with a user and temporarily display them into body-wrapper div

--- public/js/my-events.js
+++ public/js/my-events.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
 
-  // Upon loading populate the page with information associated with the user
-  // extracting user_id from the URL in the th browser address bar
-  var url = window.location.search;
+  //
+  var bodyWrapper = $("#body-wrapper1");
+  // var allEvents;
 
+  // Upon loading the page, updating the links with information associated with the user
+  var url = window.location.search;
   var userId;
   if (url.indexOf("?user_id=") !== -1) userId = url.split("=")[1];
-
   var href = "/my-events?user_id=" + userId;
   $("#my-events-link").attr("href", href);
   href = "/create-event?user_id=" + userId;
   $("#create-event-link").attr("href", href);
+
+  // Grabbing all events from DB
   getEvents(userId);
 
-  // Get all the user's events from the DB and have them added to the page
   function getEvents(userId) {
     // userId = userId || "";
     // if (userId) {
@@ -22,15 +24,84 @@ $(document).ready(function () {
 
     var userIdParam = "?user_id=" + userId;
     $.get("/api/my-events" + userIdParam, function (allEvents) {
-      $("#body-wrapper1").empty();
+      // Code to add events into the page in a tabular formats
+      // allEvents = dbEvents;
+      // displayTable();
+
+
+      bodyWrapper.empty();
       for (var i = 0; i < allEvents.length; i++) {
-        $("#body-wrapper1").append(allEvents[i].name);
-        $("#body-wrapper1").append(allEvents[i].description);
-        $("#body-wrapper1").append(allEvents[i].category);
+        bodyWrapper.append(allEvents[i].name);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].description);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].category);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].streetAdd);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].city);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].zipCode);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].spots);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].state);
+        bodyWrapper.append($("<br>"));
+        bodyWrapper.append(allEvents[i].UserM.firstName);
       }
+
 
     });
   }
 
+  function displayTable() {
+    bodyWrapper.empty();
+    var eventsArr = [];
+    for (var i = 0; i < allEvents.length; i++) {
+      eventsArr.push(createNewRow(allEvents[i]));
+    }
+    bodyWrapper.append(eventsArr);
+  }
+
+  // This function constructs a post's HTML
+  function createNewRow(post) {
+    var formattedDate = new Date(post.createdAt);
+    formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
+    var newPostCard = $("<div>");
+    newPostCard.addClass("card");
+    var newPostCardHeading = $("<div>");
+    newPostCardHeading.addClass("card-header");
+    var deleteBtn = $("<button>");
+    deleteBtn.text("x");
+    deleteBtn.addClass("delete btn btn-danger");
+    var editBtn = $("<button>");
+    editBtn.text("EDIT");
+    editBtn.addClass("edit btn btn-info");
+    var newPostTitle = $("<h2>");
+    var newPostDate = $("<small>");
+    var newPostAuthor = $("<h5>");
+    newPostAuthor.text("Written by: " + post.Author.name);
+    newPostAuthor.css({
+      float: "right",
+      color: "blue",
+      "margin-top": "-10px"
+    });
+    var newPostCardBody = $("<div>");
+    newPostCardBody.addClass("card-body");
+    var newPostBody = $("<p>");
+    newPostTitle.text(post.title + " ");
+    newPostBody.text(post.body);
+    newPostDate.text(formattedDate);
+    newPostTitle.append(newPostDate);
+    newPostCardHeading.append(deleteBtn);
+    newPostCardHeading.append(editBtn);
+    newPostCardHeading.append(newPostTitle);
+    newPostCardHeading.append(newPostAuthor);
+    newPostCardBody.append(newPostBody);
+    newPostCard.append(newPostCardHeading);
+    newPostCard.append(newPostCardBody);
+    newPostCard.data("post", post);
+    return newPostCard;
+  }
 
 });
