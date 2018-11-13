73d756338631e0de63a20b891e112f499ff97c05
Tarek ElHajjaoui <tarekel96@gmail.com>
2018-11-13 12:32:11 -0800

    fixed eslint config

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
 // Middleware
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
@@ -36,11 +38,12 @@ require("./routes/html-routes")(app);
 //   syncOptions.force = true;
 // }
 
-
-db.sequelize.sync({
-  force: true
-}).then(function () {
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+db.sequelize
+  .sync({
+    force: true
+  })
+  .then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
   });
-});
